8a30c0fdc88894fac5d2cfcaea8fb929a972ccd2
Nitin Kumar <nitinkumar16072005@gmail.com>
2026-04-14 16:46:59 +0530

    [nitin].add adding utility functions for token generation

--- backend/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE || "7d"
-  });
-
-export default generateToken;
